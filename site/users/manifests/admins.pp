c223b14aca4aa1263a528ad8a73d5f446c125291
csdumas <csdumas@gmail.com>
2016-08-25 15:38:58 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,6 +1,7 @@
 class users::admins {
       users::managed_user { 'joe': }
-      users::managed_user { 'alice':
+      users::managed_user { 'alice': }
+      users::managed_user { 'chen':
        group => 'staff',
       }
 
